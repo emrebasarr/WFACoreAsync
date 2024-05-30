8cbbd426ced946f3cc7ee7e8b81e40ae22a4e80c
emre başarır <yunu_emre_kardes@outlook.com>
2024-05-30 10:57:32 +0300

    Add project files.

---
+++ WFACoreAsync_0/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WFACoreAsync_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Location = new System.Drawing.Point(141, 109);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(75, 23);
+            this.btnTopla.TabIndex = 0;
+            this.btnTopla.Text = "Topla";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.Location = new System.Drawing.Point(377, 109);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(75, 23);
+            this.btnCikar.TabIndex = 1;
+            this.btnCikar.Text = "Cikar";
+            this.btnCikar.UseVisualStyleBackColor = true;
+            this.btnCikar.Click += new System.EventHandler(this.btnCikar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCikar);
+            this.Controls.Add(this.btnTopla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Button btnCikar;
+    }
+}
+
---
+++ WFACoreAsync_0/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFACoreAsync_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Topla(int sayi, int sayi2)
+        {
+            Thread.Sleep(5000);
+            MessageBox.Show($"{sayi+sayi2}");
+        }
+
+        public void Cikar(int sayi, int sayi2)
+        {
+            MessageBox.Show($"{sayi - sayi2}");
+        }
+
+        Task ToplaAsync(int sayi, int sayi2)
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                //MessageBox.Show($"{sayi + sayi2}");
+
+                //this.Invoke(() =>
+                //{
+                //    Text = $"{sayi + sayi2}";
+                //});
+            });
+        }
+
+        private async void btnTopla_Click(object sender, EventArgs e)
+        {
+            //Topla(3, 3);
+            await ToplaAsync(3, 3);
+            MessageBox.Show("Toplama Görevi başarılı bir şekilde gerçekleştirildi");
+        }
+
+        private void btnCikar_Click(object sender, EventArgs e)
+        {
+            Cikar(5, 3);
+        }
+    }
+}
